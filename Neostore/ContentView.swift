36f065008a7ad706fd86fdbc168c5dc7a1d39d40
Ashwini <ashwini.vishwas@neosofttech.com>
2020-03-27 10:09:34 +0530

    Initial Commit

---
+++ Neostore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Neostore
+//
+//  Created by Neosoft on 27/03/20.
+//  Copyright © 2020 Neosoft. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
